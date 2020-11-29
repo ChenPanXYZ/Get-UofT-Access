ebfd719afecd22d9ab8f3c475e9e648febffafcf
Pan Chen <pan@fzyzer.com>
2020-11-28 20:09:47 -0500

    firefox support, fake request

--- background.js
+++ background.js
@@ -1,19 +1,43 @@
 // background.js
 
-// Check if the user clicks the brower button.
-chrome.browserAction.onClicked.addListener(function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        let activeTab = tabs[0]
-        chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" })
-    })
-})
+// Check if it's chrome(edge) or firefox.
+let goodthing = undefined
+if (chrome !== undefined) {
+    goodthing = chrome
+}
+else if (browser !== undefined) {
+    goodthing = browser
+}
 
 
-chrome.runtime.onMessage.addListener(
-    function (request) {
-        if (request.message === "redirect_to_uoft_access") {
-            // Update the URL to redirect
-            chrome.tabs.update({"url": request.url})
+function UrlWithUofTAccessMaker(domain, url) {
+    // Actually, just replace the first occur of the domain would be fine, because the very first part in a url must be the domain.
+
+    return url.replace(domain, domain.replaceAll(".", "-") + ".myaccess.library.utoronto.ca")
+    //return url.replace(/(http:|)(^|\/\/)(.*?\/)/g, 'https://' + domain.replaceAll(".", "-") + ".myaccess.library.utoronto.ca")
+}
+
+
+// Only if the user is using a compatible browser.
+if (goodthing !== undefined) {
+    // Check if the user clicks the brower button.
+    goodthing.browserAction.onClicked.addListener((tab) => {
+        const url = new URL(tab.url)
+        let xhr = new XMLHttpRequest()
+        // We don't do redirections for homepages.
+        if (url.pathname !== "/") {
+            const UofTAccessLink = UrlWithUofTAccessMaker(url.hostname, url.href)
+            // Send a fake request to see if UofT has access to this page.
+            xhr.open("GET", UofTAccessLink, true)
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status !== 404) {
+                        // If the UofT has the access to this page.
+                        goodthing.tabs.update({"url": UofTAccessLink})
+                    }
+                }
+            }
+            xhr.send()
         }
-    }
-)
+    })
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+## Installation
+### Download
+Download the `get-uoft-access.zip`.
+### Chrome
+1. Unzip the `downloaded .zip file`. There should be a folder named `get-uoft-access`.
+2. In your Chrome, go to `Settings -> Extensions`.
+3. Make sure that `Developer Mode` on the top right is enabled.
+4. Click `Load unpacked`.
+5. Select the `get-uoft-access folder`.
+6. `Get UofT Access` is installed, By default, `Get UofT Access` is unpinned, please follow this instruction to pin it for your convenience: https://www.maketecheasier.com/pin-extensions-to-chrome-toolbar/
+### Edge
+1. Unzip the `downloaded .zip file`. There should be a folder named `get-uoft-access`.
+2. In your Edge, go to `Extensions`.
+3. Make sure that `Developer Mode` on the bottom left is enabled.
+4. Click `Load unpacked`.
+5. Select the `get-uoft-access` folder.
+6. `Get UofT Access` is installed.
+### Firefox
+1. Do not unzip the `downloaded .zip file`.
+2. In your Firefox, go to `about:debugging`.
+3. Switch to `This Firefox`.
+4. Click `Load Temporary Add-on...`.
+5. Select the `downloaded .zip file`.
+6. `Get UofT Access` is installed.
+## How to use
+At any web page, click the `Get UofT Access` button in the toolbar.
+You will get the following results:
+1. If it is the homepage of a website OR that UofT doesn't have access to this web page, nothing will happen in your side.
+2. If it is not the homepage of a website AND UofT has access to this website, you will be redirected to the corresponding page under `myaccess.library.utoronto.ca`, which UofT members have access to.
